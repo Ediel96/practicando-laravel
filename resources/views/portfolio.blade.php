2d4a99d837b176aae3c8a9086396f6ff5b681108
DarkEdiel96 <39193308+Ediel96@users.noreply.github.com>
2020-06-21 23:58:45 -0500

    Agregando blade id

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -4,4 +4,17 @@
 
 @section('content')
     <h1>Portafolio</h1>
+        
+    <lu>
+        @if($portfolio)
+            @foreach ($portfolio as $portfolioItem)
+
+                <li> {{ $portfolioItem['title'] }} </li>;
+            @endforeach
+        @else
+            <li>No hay proyectos para mostrar</li>
+        @endif 
+    </lu>
+
 @endsection
+
