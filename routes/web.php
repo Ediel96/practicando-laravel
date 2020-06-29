e919514aa7af9c487ce06d12eb693887ef1ee006
DarkEdiel96 <39193308+Ediel96@users.noreply.github.com>
2020-06-29 01:01:54 -0500

    agregando PortfolioControler _invoke

--- routes/web.php
+++ routes/web.php
@@ -28,14 +28,12 @@ Route::get('/', function () {
 */
 
 
-$portfolio = [
-    ['title' => 'Proyecto #1'],
-    //['title' => 'Proyecto #2'],
-    //['title' => 'Proyecto #3'],
-    //['title' => 'Proyecto #4'],
-];
 
 Route::view('/','home')->name('home');
 Route::view('/about','about')->name('about');
-Route::view('/portfolio','portfolio', compact('portfolio'))->name('portfolio');
+
+
+Route::get('/portfolio', 'PortfolioController');
+
+
 Route::view('/contact','contact')->name('contact');
